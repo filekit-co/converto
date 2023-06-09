4e232354815447eb4ce042f7f5a39b20b73d6ed4
minkj1992 <minkj1992@gmail.com>
2023-06-09 14:52:54 +0900

    feat: init daisyUI + tailwindcss + sveltekit +  postcss setting

--- README.md
+++ README.md
@@ -13,7 +13,12 @@
 ```bash
 # alias pn=pnpm
 pn create svelte@latest converto
+pn i -D tailwindcss postcss autoprefixer daisyui
+npx tailwindcss init -p
 ```
+
+- [Docs: Install Tailwind CSS with SvelteKit](https://tailwindcss.com/docs/guides/sveltekit)
+- [Docs: Install daisyUI as a Tailwind CSS plugin](https://daisyui.com/docs/install/)
 
 ## dev
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello Daisy</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
