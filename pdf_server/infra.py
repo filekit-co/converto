dc45093ca13adc2411fb05355ef89cf176ac9686
Minwook Je <37536298+minkj1992@users.noreply.github.com>
2023-06-20 10:58:30 +0900

    feat: encrypt and decrypt pdf (#7)

--- pdf_server/infra.py
+++ pdf_server/infra.py
@@ -1,4 +1,6 @@
+import io
 import logging
+from typing import Optional
 
 import fitz
 from docx import Document
@@ -108,19 +110,79 @@ class DocxConverter(Converter):
         docx_file.save(path_or_stream=self.path_or_stream)
 
 
-class ConversionException(Exception): 
-    pass
-
-class MakedocxException(ConversionException): 
-    pass
-
-
-
 def convert_bytes_to_pdf(stream: bytes, input_type: str):
     """
     You can access files with extensions like .xps, .oxps, .cbz, .fb2 or .epub
 
     ref: https://pymupdf.readthedocs.io/en/latest/document.html#Document.convert_to_pdf
     """
-    doc  = fitz.Document(stream=stream, filetype=input_type)
-    return doc.convert_to_pdf()
+    # TODO: test 
+    with fitz.Document(stream=stream, filetype=input_type) as doc:
+        return doc.convert_to_pdf()
+
+
+perm = int(
+	fitz.PDF_PERM_ACCESSIBILITY # always use this
+	| fitz.PDF_PERM_PRINT # permit printing
+	| fitz.PDF_PERM_COPY # permit copying
+	| fitz.PDF_PERM_ANNOTATE # permit annotations
+)
+encrypt_meth = fitz.PDF_ENCRYPT_AES_256 # strongest algorithm
+def encrypt_pdf(file_bytes: bytes, ro_password: str, rw_password: Optional[str] = None):
+    # The owner password provides full access rights, including changing passwords, encryption method, or permission detail.
+    # The user password provides access to document content according to the established permission details. If present, opening the PDF in a viewer will require providing it.
+    # !IMPORTANT: The user password cannot remove pdf password.
+
+    out_bytes = io.BytesIO()
+    with fitz.Document(stream=file_bytes, filetype='pdf') as doc:
+        if doc.needs_pass:
+            raise AlreadyEncryptException(f'This pdf is already encrypted.')
+
+        doc.save(
+            out_bytes,
+            encryption=encrypt_meth, # set the encryption method
+            user_pw=ro_password, # set the user password            
+            owner_pw=rw_password, # set the owner password
+            permissions=perm, # set permissions
+        )
+        return out_bytes.getvalue()
+
+
+
+def decrypt_pdf(file_bytes: bytes, password:str):
+    def is_incorrect_password_and_decrypt():
+        # .authenticate() decrypt document
+        try:
+            return doc.authenticate(password) <= 0
+        except Exception as e:
+            logging.error(e)
+            doc.close()
+            return True
+    
+    out_bytes = io.BytesIO()
+    with fitz.Document(stream=file_bytes, filetype='pdf') as doc:
+        if not doc.needs_pass:
+            return doc.tobytes()
+
+        if is_incorrect_password_and_decrypt():
+            raise InvalidPasswordException(f'Invalid password is given {password}')
+        
+        doc.save(out_bytes)
+        return out_bytes.getvalue()
+
+
+
+
+class ConversionException(Exception): 
+    pass
+
+class MakedocxException(ConversionException): 
+    pass
+
+class InvalidPasswordException(ConversionException):
+    pass
+
+
+class AlreadyEncryptException(ConversionException):
+    pass
+
