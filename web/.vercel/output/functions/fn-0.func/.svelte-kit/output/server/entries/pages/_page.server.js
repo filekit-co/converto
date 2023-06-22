095106b68fc4f2362563b2d2f11b0cf87b6802c8
seilylook <seilylook@naver.com>
2023-06-22 17:27:02 +0900

    error: pnpm-lock 에러로 인해 임시 삭제

--- web/.vercel/output/functions/fn-0.func/.svelte-kit/output/server/entries/pages/_page.server.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  isr: {
-    expiration: 60
-  }
-};
-export {
-  config
-};
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import {vitePreprocess} from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 
@@ -13,9 +13,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      pages: 'build'
-    }),
+    adapter: adapter(),
     alias: {
       '@src': path.resolve('/src'),
       '@components': path.resolve('src/lib/components'),
