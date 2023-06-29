e152cb00ae9cb085ac1a990d5a7caf81a3256f75
minkj1992 <minkj1992@gmail.com>
2023-06-29 10:55:03 +0900

    feat: sitemap generator

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,8 @@ import '@store/i18n';
 import {locale, waitLocale} from 'svelte-i18n';
 import type {Config} from '@sveltejs/adapter-vercel';
 
+export const prerender = true
+
 export const config: Config = {
   runtime: 'nodejs18.x'
 };
