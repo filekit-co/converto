4df99ff56f887e04dba39b2a40dbfb4de9956a79
seilylook <seilylook@naver.com>
2023-06-29 21:03:06 +0900

    design: 자잘한 폰트 수정

--- src/lib/components/common/ImgExample.svelte
+++ src/lib/components/common/ImgExample.svelte
@@ -45,162 +45,39 @@
           <div
             class="mx-auto w-full h-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="human-origin.jpg" />
+            <img src="images/human-origin.jpg" />
 
-            <img src="human-result.png" />
+            <img src="images/human-result.png" />
           </div>
         {/if}
         {#if buttonState === 'stuff'}
           <div
             class="mx-auto w-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="stuff-origin.jpg" />
+            <img src="images/stuff-origin.jpg" />
 
-            <img src="stuff-result.png" />
+            <img src="images/stuff-result.png" />
           </div>
         {/if}
         {#if buttonState === 'animal'}
           <div
             class="mx-auto w-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="animal-origin.jpg" />
+            <img src="images/animal-origin.jpg" />
 
-            <img src="animal-result.png" />
+            <img src="images/animal-result.png" />
           </div>
         {/if}
         {#if buttonState === 'character'}
           <div
             class="mx-auto w-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="character-origin.jpg" />
+            <img src="images/character-origin.jpg" />
 
-            <img src="character-result.png" />
+            <img src="images/character-result.png" />
           </div>
         {/if}
       </div>
     </div>
   </div>
 </div>
-
-<style>
-  .checkerboard {
-    --checkerboard-black-color: #f8f9f9;
-    --checkerboard-white-color: transparent;
-    background-image: linear-gradient(
-        45deg,
-        var(--checkerboard-black-color) 25%,
-        var(--checkerboard-white-color) 25%
-      ),
-      linear-gradient(
-        -45deg,
-        var(--checkerboard-black-color) 25%,
-        var(--checkerboard-white-color) 25%
-      ),
-      linear-gradient(
-        45deg,
-        var(--checkerboard-white-color) 75%,
-        var(--checkerboard-black-color) 75%
-      ),
-      linear-gradient(
-        -45deg,
-        var(--checkerboard-white-color) 75%,
-        var(--checkerboard-black-color) 75%
-      );
-    background-position: 0 0, 0 10px, 10px -10px, -10px 0;
-    background-size: 20px 20px;
-  }
-  .prism-imagedragger input[type='range'] {
-    margin-left: -1.5rem;
-    position: relative;
-    width: calc(100% + 3rem);
-  }
-
-  .prism-imagedragger input[type='range']:focus-visible::-webkit-slider-thumb {
-    border: 6px solid #0f70e6;
-  }
-
-  .prism-imagedragger input[type='range']::-webkit-slider-runnable-track {
-    background: transparent;
-  }
-
-  .prism-imagedragger input[type='range']::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    background: rgba(45, 45, 45, 0.75);
-    background-image: url(/imagedragger-thumb-arrows.svg);
-    background-position: 50%;
-    background-repeat: no-repeat;
-    border: 2px solid #fff;
-    border-radius: 9999px;
-    box-shadow: 0 0 56px #45454533;
-    cursor: -webkit-grab;
-    cursor: grab;
-    height: 3rem;
-    -webkit-transition: ease-in-out;
-    transition: ease-in-out;
-    transition-duration: 0.1s;
-    width: 3rem;
-  }
-
-  .prism-imagedragger input[type='range']::-moz-range-thumb {
-    -webkit-appearance: none;
-    background: rgba(45, 45, 45, 0.75);
-    background-image: url(/imagedragger-thumb-arrows.svg);
-    background-position: 50%;
-    background-repeat: no-repeat;
-    border: 2px solid #fff;
-    border-radius: 9999px;
-    box-shadow: 0 0 56px #45454533;
-    cursor: grab;
-    height: 3rem;
-    -moz-transition: ease-in-out;
-    transition: ease-in-out;
-    transition-duration: 0.1s;
-    width: 3rem;
-  }
-
-  .prism-imagedragger input[type='range']::-ms-thumb {
-    -webkit-appearance: none;
-    background: rgba(45, 45, 45, 0.75);
-    background-image: url(/imagedragger-thumb-arrows.svg);
-    background-position: 50%;
-    background-repeat: no-repeat;
-    border: 2px solid #fff;
-    border-radius: 9999px;
-    box-shadow: 0 0 56px #45454533;
-    cursor: grab;
-    height: 3rem;
-    -ms-transition: ease-in-out;
-    transition: ease-in-out;
-    transition-duration: 0.1s;
-    width: 3rem;
-  }
-
-  .prism-imagedragger input[type='range']::-webkit-slider-thumb:active,
-  .prism-imagedragger input[type='range']::-webkit-slider-thumb:focus,
-  .prism-imagedragger input[type='range']::-webkit-slider-thumb:hover {
-    transform: scale(1.2);
-    transform-origin: center center;
-  }
-
-  .prism-imagedragger input[type='range']::-webkit-slider-thumb:active,
-  .prism-imagedragger input[type='range']::-webkit-slider-thumb:focus {
-    cursor: -webkit-grabbing;
-    cursor: grabbing;
-  }
-
-  .prism-imagedragger input[type='range'][disabled]::-webkit-slider-thumb {
-    display: none;
-  }
-
-  .prism-imagedragger input[type='range'][disabled]::-moz-range-thumb {
-    display: none;
-  }
-
-  .prism-imagedragger input[type='range'][disabled]::-ms-thumb {
-    display: none;
-  }
-  .prism .animate-pulse {
-    -webkit-animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
-    animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
-  }
-</style>
