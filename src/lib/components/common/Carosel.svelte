a374ff3cc5e44ec3d80024b721096e91d85e475a
seilylook <seilylook@naver.com>
2023-06-29 23:08:31 +0900

    feat: remove-background 예시 컨텐츠 추가

---
+++ src/lib/components/common/Carosel.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import {scrollIntoView} from './scrollView';
+
+  const images = [
+    {title: 'Person Before', src: '/images/carosel/human.jpg'},
+    {title: 'Person After', src: '/images/carosel/human-result.png'},
+    {title: 'Animal Before', src: '/images/carosel/animal.jpg'},
+    {title: 'Animal After', src: '/images/carosel/animal-result.png'},
+    {title: 'Stuff Before', src: '/images/carosel/stuff.jpg'},
+    {title: 'Stuff After', src: '/images/carosel/stuff-result.png'},
+    {title: 'Car Before', src: '/images/carosel/car.jpg'},
+    {title: 'Car After', src: '/images/carosel/car-result.png'}
+  ];
+
+  let name = 'carosel';
+  const getItemId = (index: number, carouselName = name) =>
+    `${carouselName}-item-${index}`;
+</script>
+
+<div class="py-4 md:py-6 overflow-x-clip mb-8">
+  <div class="mx-auto w-full px-8 max-w-5xl max-h-3xl">
+    <div class="py-4 md:py-8">
+      <h2
+        class="font-display font-bold text-typo m-0 text-4xl md:text-5xl text-center"
+      >
+        Removed Background Image Example
+      </h2>
+    </div>
+
+    <ul
+      class="flex overflow-x-auto gap-6 snap-x snap-mandatory before:shrink-0 before:w-[30vw] after:shrink-0 after:w-[30vw] pb-6 bg-gray-100 rounded"
+    >
+      {#each images as { title, src }, index}
+        <li id={getItemId(index)} class="shrink-0 snap-center shadow-lg">
+          <img {src} alt={title} class="rounded" style="height: 500px" />
+          <div
+            class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2"
+          />
+        </li>
+      {/each}
+    </ul>
+  </div>
+</div>
--- src/lib/components/common/ImgExample.svelte
+++ src/lib/components/common/ImgExample.svelte
@@ -5,7 +5,7 @@
   }
 </script>
 
-<div class="py-4 md:py-6 overflow-x-clip">
+<div class="py-4 md:py-6 overflow-x-cli">
   <div class="mx-auto w-full px-8 max-w-5xl">
     <div class="py-4 md:py-8">
       <h2
@@ -38,43 +38,43 @@
     >
   </div>
 
-  <div class="mx-auto w-full px-8 max-w-5xl">
-    <div class="relative group">
+  <div class="mx-auto w-full px-8 max-w-5xl hide-scrollbars">
+    <div class="relative group bg-gray-100 mb-8 rounded-2xl">
       <div class=" py-4 md:py-8">
         {#if buttonState === 'human'}
           <div
             class="mx-auto w-full h-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="images/human-origin.jpg" />
+            <img src="images/human-origin.jpg" class="rounded" />
 
-            <img src="images/human-result.png" />
+            <img src="images/human-result.png" class="rounded" />
           </div>
         {/if}
         {#if buttonState === 'stuff'}
           <div
             class="mx-auto w-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="images/stuff-origin.jpg" />
+            <img src="images/stuff-origin.jpg" class="rounded" />
 
-            <img src="images/stuff-result.png" />
+            <img src="images/stuff-result.png" class="rounded" />
           </div>
         {/if}
         {#if buttonState === 'animal'}
           <div
             class="mx-auto w-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="images/animal-origin.jpg" />
+            <img src="images/animal-origin.jpg" class="rounded" />
 
-            <img src="images/animal-result.png" />
+            <img src="images/animal-result.png" class="rounded" />
           </div>
         {/if}
         {#if buttonState === 'character'}
           <div
             class="mx-auto w-full px-8 max-w-7xl overflow-x-scroll md:overflow-auto px-8 scroll-pl-8 md:scroll-p-0 flex flex-nowrap gap-6 md:grid md:grid-cols-2"
           >
-            <img src="images/character-origin.jpg" />
+            <img src="images/character-origin.jpg" class="rounded" />
 
-            <img src="images/character-result.png" />
+            <img src="images/character-result.png" class="rounded" />
           </div>
         {/if}
       </div>
