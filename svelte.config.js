48eba9818e3c70127eb5a697bbee08dd1f1e43c0
minkj1992 <minkj1992@gmail.com>
2023-06-30 23:23:46 +0900

    deploy: tidify dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import * as path from 'path';
 import adapter from '@sveltejs/adapter-cloudflare';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  // preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter({ routes: { include: ['/*'], exclude: ['<all>', '/sitemap.xml'] } }),
