d6e88370b8c2b74e65e501a127a5931b32e86c77
minkj1992 <minkj1992@gmail.com>
2023-06-30 21:15:09 +0900

    feat: cbz, fb2, oxps to pdf

---
+++ src/routes/cbz-to-pdf/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import DragDrop from '@components/common/DragDrop.svelte';
+  import {onDestroy} from 'svelte';
+  import type {FileDropOptions, Files} from 'filedrop-svelte';
+  import {_} from 'svelte-i18n';
+  import Downloader from '@components/common/Downloader.svelte';
+
+  import {fetchCbzToPdf} from '$lib/apis'; // TODO: change
+  import BaseUploader from '@components/FileUploaders/FileUploader.svelte';
+  import type {fileUploadData} from '@components/FileUploaders/types';
+
+  let fileDropOptions: FileDropOptions = {
+    accept: ['.cbz'], // TODO: change
+    hideInput: true,
+    multiple: true
+  };
+  let files: Files = {
+    accepted: [],
+    rejected: []
+  };
+
+  let uploadData: fileUploadData = [];
+  $: isDownloading = false;
+  $: isFileExist = files.accepted.length > 0;
+
+  onDestroy(() => {
+    files = {
+      accepted: [],
+      rejected: []
+    };
+  });
+</script>
+
+<div class="w-full px-2 py-32 sm:px-20 lg:px-32 text-center mx-auto">
+  <div class="join">
+    <img
+      src="/images/cbz.png"
+      class="w-12 md-16 lg:w-32 join-item"
+      alt="cbz-logo"
+    />
+
+    <h2 class="text-[clamp(2rem,6vw,4.5rem)] font-black join-item">
+      <!-- TODO: change text and color -->
+      CBZ to&nbsp<span class="text-[#ab7827]">PDF</span>
+    </h2>
+  </div>
+  <!-- TODO: change text -->
+  <p class="text-base-content/60 font-title py-4 font-light md:text-2xl">
+    Convert CBZ to PDF online for free
+  </p>
+
+  <section class="pt-4 mx-0 sm:mx-10">
+    {#if isFileExist}
+      {#if isDownloading}
+        <Downloader fetchFn={fetchCbzToPdf} {uploadData} />
+      {:else}
+        <BaseUploader bind:files bind:uploadData bind:isDownloading />
+      {/if}
+    {:else}
+      <DragDrop bind:files {fileDropOptions} />
+    {/if}
+  </section>
+</div>
---
+++ src/routes/fb2-to-pdf/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import DragDrop from '@components/common/DragDrop.svelte';
+  import {onDestroy} from 'svelte';
+  import type {FileDropOptions, Files} from 'filedrop-svelte';
+  import {_} from 'svelte-i18n';
+  import Downloader from '@components/common/Downloader.svelte';
+
+  import {fetchFb2ToPdf} from '$lib/apis';
+  import BaseUploader from '@components/FileUploaders/FileUploader.svelte';
+  import type {fileUploadData} from '@components/FileUploaders/types';
+
+  let fileDropOptions: FileDropOptions = {
+    accept: ['.fb2'],
+    hideInput: true,
+    multiple: true
+  };
+  let files: Files = {
+    accepted: [],
+    rejected: []
+  };
+
+  let uploadData: fileUploadData = [];
+  $: isDownloading = false;
+  $: isFileExist = files.accepted.length > 0;
+
+  onDestroy(() => {
+    files = {
+      accepted: [],
+      rejected: []
+    };
+  });
+</script>
+
+<div class="w-full px-2 py-32 sm:px-20 lg:px-32 text-center mx-auto">
+  <div class="join">
+    <h2 class="text-[clamp(2rem,6vw,4.5rem)] font-black join-item">
+      Fb2 to&nbsp<span class="text-[#f58638]">PDF</span>
+    </h2>
+  </div>
+  <p class="text-base-content/60 font-title py-4 font-light md:text-2xl">
+    Convert FictionBook type(.fb2) to PDF online for free
+  </p>
+
+  <section class="pt-4 mx-0 sm:mx-10">
+    {#if isFileExist}
+      {#if isDownloading}
+        <Downloader fetchFn={fetchFb2ToPdf} {uploadData} />
+      {:else}
+        <BaseUploader bind:files bind:uploadData bind:isDownloading />
+      {/if}
+    {:else}
+      <DragDrop bind:files {fileDropOptions} />
+    {/if}
+  </section>
+</div>
