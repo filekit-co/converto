d2a9d45c83507d18347ca2b467a3f52218696da9
minkj1992 <minkj1992@gmail.com>
2023-07-03 15:55:04 +0900

    feat: nav and drawer with close

---
+++ src/lib/components/common/DrawSide.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import {IconX} from '@tabler/icons-svelte';
+  import {_} from 'svelte-i18n';
+  import {checked, handleOnClose} from '@store/nav';
+
+  export let navId: string;
+</script>
+
+<!-- TODO: fill sidebar -->
+<!-- drawer side content -->
+<div class="drawer-side">
+  <label for={navId} class="drawer-overlay" />
+  <ul class="menu p-4 w-80 h-full bg-base-200">
+    <h5
+      class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
+    >
+      Menu
+    </h5>
+    <button
+      type="button"
+      on:click={handleOnClose}
+      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+    >
+      <IconX class="w-5 h-5" />
+      <span class="sr-only">Close menu</span>
+    </button>
+    <!-- Sidebar content here -->
+  </ul>
+</div>
--- src/lib/components/common/Nav.svelte
+++ src/lib/components/common/Nav.svelte
@@ -2,33 +2,36 @@
   import {IconMenu2} from '@tabler/icons-svelte';
   import {_} from 'svelte-i18n';
   import Flags from '@components/common/Flags.svelte';
+  import DrawSide from '@components/common/DrawSide.svelte';
+  import {checked} from '@store/nav';
 
   const navId = 'filekit-nav';
 </script>
 
-<div class="drawer z-10">
-  <input id={navId} type="checkbox" class="drawer-toggle" />
+<div class="drawer drawer-mobile z-10">
+  <input
+    id={navId}
+    bind:checked={$checked}
+    type="checkbox"
+    class="drawer-toggle"
+  />
   <div class="drawer-content flex flex-col">
     <!-- Navbar -->
-    <div class="w-full navbar bg-base-300">
-      <a class="flex-none btn btn-ghost" href="/" title="filekit">
-        <img src="/images/paper_folder.png" class="h-12" alt="Filekit Logo" />
-        <span class="normal-case text-xl">Filekit</span>
-      </a>
-
-      <div class="flex-1">
-        <Flags />
-      </div>
-      <!-- Divider -->
-      <div class="flex-1 px-2 mx-2" />
-      <div class="flex-none md:hidden">
+    <div class="w-full navbar bg-base-300 drop-shadow-lg">
+      <div class="flex-none lg:hidden">
         <label for={navId} class="btn btn-square btn-ghost">
           <IconMenu2 class="inline-block w-6 h-6 stroke-current" />
         </label>
       </div>
 
-      <div class="flex-none hidden lg:block">
-        <ul class="menu menu-horizontal">
+      <a class="flex-none btn btn-ghost" href="/" title="filekit">
+        <img src="/images/paper_folder.png" class="h-12" alt="Filekit Logo" />
+        <span class="normal-case text-xl">Filekit</span>
+      </a>
+
+      <!-- Divider -->
+      <div class="flex-1 px-2 mx-2">
+        <ul class="invisible lg:visible menu menu-horizontal">
           <!-- Navbar menu content here -->
           <!-- TODO: link it -->
           <li><a href="/pdf">PDF</a></li>
@@ -41,14 +44,5 @@
     </div>
   </div>
 
-  <!-- TODO: fill sidebar -->
-  <!-- drawer side content -->
-  <div class="drawer-side">
-    <label for={navId} class="drawer-overlay" />
-    <ul class="menu p-4 w-80 h-full bg-base-200">
-      <!-- Sidebar content here -->
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  </div>
+  <DrawSide {navId} />
 </div>
