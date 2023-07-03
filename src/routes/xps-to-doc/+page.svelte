bbbe6f6dda33eab702b984cb42fddfa3e8a9a847
minkj1992 <minkj1992@gmail.com>
2023-07-03 22:47:30 +0900

    chore: delete unpretty icon

--- src/routes/xps-to-doc/+page.svelte
+++ src/routes/xps-to-doc/+page.svelte
@@ -33,12 +33,6 @@
 
 <div class="w-full px-2 py-32 sm:px-20 lg:px-32 text-center mx-auto">
   <div class="join">
-    <img
-      src="/images/xps.png"
-      class="w-12 md-16 lg:w-32 join-item"
-      alt="xps logo"
-    />
-
     <h2 class="text-[clamp(2rem,6vw,4.5rem)] font-black join-item">
       <!-- TODO: change text and color -->
       XPS to&nbsp<span class="text-[#37CDBE]">DOC</span>
--- src/routes/xps-to-docx/+page.svelte
+++ src/routes/xps-to-docx/+page.svelte
@@ -33,12 +33,6 @@
 
 <div class="w-full px-2 py-32 sm:px-20 lg:px-32 text-center mx-auto">
   <div class="join">
-    <img
-      src="/images/xps.png"
-      class="w-12 md-16 lg:w-32 join-item"
-      alt="xps logo"
-    />
-
     <h2 class="text-[clamp(2rem,6vw,4.5rem)] font-black join-item">
       XPS to&nbsp<span class="text-[#37CDBE]">DOCX</span>
     </h2>
