578cf691c81bdcca185da479d1beb7890c6c21a3
minkj1992 <minkj1992@gmail.com>
2023-07-05 23:02:05 +0900

    feat: add image pages to nav and footer

--- src/routes/img/[slug]/+page.ts
+++ src/routes/img/[slug]/+page.ts
@@ -5,7 +5,7 @@ import type { fileUploadData } from '@components/FileUploaders/types';
 import { getImgFetchApi } from '$lib/apis';
 import { imgExts, imgSlugToHex, imgSlugs } from '$lib/data';
 import type { UpdateHeaderProps } from '$lib/types';
-import { canonicalUrl } from '$lib/utils';
+import { canonicalUrl, extractExtsFromString } from '$lib/utils';
 
 type ConversionProps = {
   title: string;
@@ -22,25 +22,11 @@ const imgFileDropOptions: FileDropOptions = {
   multiple: true
 }
 
-function extractExtsFromSlug(slug: string): [string, string] {
-  
-  const regex = /(\w+)-to-(\w+)/;
-  const matches = slug.match(regex);
-
-  // check 3 group i.g [png, to, jpeg]
-  if (matches && matches.length === 3) {
-    const ext1 = matches[1];
-    const ext2 = matches[2];
-    return [ext1, ext2];
-  }
-
-  throw new Error('Invalid slug format');
-}
 
 export const load = (({ params, route }) => {
   if (!imgSlugs.includes(params.slug)) throw error(404, 'Not found');
   
-  const [fromExt, toExt] = extractExtsFromSlug(params.slug)
+  const [fromExt, toExt] = extractExtsFromString(params.slug)
   const titleAndDescription = `Convert ${fromExt.toUpperCase()} to ${toExt.toUpperCase()} online for free`
   const emphasizeColor: string = imgSlugToHex[params.slug]
 
