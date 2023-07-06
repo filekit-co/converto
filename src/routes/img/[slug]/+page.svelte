575a15c8e8b137d8e49c8c11cb7ccff2cd18c80c
minkj1992 <minkj1992@gmail.com>
2023-07-06 14:03:45 +0900

    fix: #66

--- src/routes/img/[slug]/+page.svelte
+++ src/routes/img/[slug]/+page.svelte
@@ -1,24 +1,34 @@
 <script lang="ts">
   import DragDrop from '@components/common/DragDrop.svelte';
   import {onDestroy} from 'svelte';
-  import type {Files} from 'filedrop-svelte';
+  import type {FileDropOptions, Files} from 'filedrop-svelte';
   import {_} from 'svelte-i18n';
   import Downloader from '@components/common/Downloader.svelte';
   import BaseUploader from '@components/FileUploaders/FileUploader.svelte';
   import type {fileUploadData} from '@components/FileUploaders/types';
 
   import type {PageData} from './$types';
+  import {fetchToY} from '$lib/apis';
 
   export let data: PageData;
-
   let files: Files = {
     accepted: [],
     rejected: []
   };
 
+  const [fromExt, toExt] = data.exts;
+  const imgFileDropOptions: FileDropOptions = {
+    accept: ['.' + fromExt],
+    hideInput: true,
+    multiple: true
+  };
+
   let uploadData: fileUploadData = [];
   $: isDownloading = false;
   $: isFileExist = files.accepted.length > 0;
+
+  const handleDownload = async (data: fileUploadData) =>
+    await fetchToY(data, toExt);
 
   onDestroy(() => {
     files = {
@@ -31,7 +41,7 @@
 <div class="w-full px-2 py-32 sm:px-20 lg:px-32 text-center mx-auto">
   <div class="join">
     <h2 class="text-[clamp(2rem,6vw,4.5rem)] font-black join-item">
-      {@html data.title}
+      {@html data.xToy}
     </h2>
   </div>
   <p class="text-base-content/60 font-title py-4 font-light md:text-2xl">
@@ -41,12 +51,12 @@
   <section class="pt-4 mx-0 sm:mx-10">
     {#if isFileExist}
       {#if isDownloading}
-        <Downloader fetchFn={data.fetchApi} {uploadData} />
+        <Downloader fetchFn={handleDownload} {uploadData} />
       {:else}
         <BaseUploader bind:files bind:uploadData bind:isDownloading />
       {/if}
     {:else}
-      <DragDrop bind:files fileDropOptions={data.fileDropOptions} />
+      <DragDrop bind:files fileDropOptions={imgFileDropOptions} />
     {/if}
   </section>
 </div>
--- src/routes/img/[slug]/+page.ts
+++ src/routes/img/[slug]/+page.ts
@@ -1,34 +1,22 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
-import type { FileDropOptions } from 'filedrop-svelte';
-import type { fileUploadData } from '@components/FileUploaders/types';
-import { getImgFetchApi } from '$lib/apis';
-import { imgExts, imgSlugToHex, imgSlugs } from '$lib/data';
+import { imgSlugToHex, imgSlugs } from '$lib/data';
 import type { UpdateHeaderProps } from '$lib/types';
 import { canonicalUrl, extractExtsFromString } from '$lib/utils';
 
 type ConversionProps = {
-  title: string;
+  xToy: string;
   description: string;
-  fetchApi: (data: fileUploadData) => Promise<any>;
-  fileDropOptions: FileDropOptions;
+  exts: [string, string];
 };
-
-
-const fileAccepts = imgExts.filter((x) => x!= 'ps').map((imgExt) => `.${imgExt}`)
-const imgFileDropOptions: FileDropOptions = {
-  accept: fileAccepts,
-  hideInput: true,
-  multiple: true
-}
-
 
 export const load = (({ params, route }) => {
   if (!imgSlugs.includes(params.slug)) throw error(404, 'Not found');
   
   const [fromExt, toExt] = extractExtsFromString(params.slug)
   const titleAndDescription = `Convert ${fromExt.toUpperCase()} to ${toExt.toUpperCase()} online for free`
-  const emphasizeColor: string = imgSlugToHex[params.slug]
+  const fontColor = imgSlugToHex[params.slug]
+  const xToy: string = `${fromExt.toUpperCase()} to&nbsp;<span style=color:${fontColor};>${toExt.toUpperCase()}</span>`
 
   const headerProps: UpdateHeaderProps = {
     title: titleAndDescription,
@@ -38,9 +26,8 @@ export const load = (({ params, route }) => {
   };
   return {
     headerProps,
-    title: `${fromExt.toUpperCase()} to&nbsp;<span class="text-[${emphasizeColor}]">${toExt.toUpperCase()}</span>`,
+    xToy, 
     description: titleAndDescription,
-    fetchApi: getImgFetchApi(fromExt, toExt),
-    fileDropOptions: imgFileDropOptions,
+    exts: [fromExt, toExt]
   } as ConversionProps
 }) satisfies PageLoad;
