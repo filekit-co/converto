fb5f7ec67803b82846ad2a0aecba3fe91c234737
minkj1992 <minkj1992@gmail.com>
2023-07-08 12:56:26 +0900

    I REALLY HATE KAKAO IN APP BROWSER

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,19 @@
 import { languages } from '$lib/data';
 import { getRuntimeFromLocals, addRuntimeToLocals, initSvelteKitServerRuntime } from '@inlang/sdk-js/adapter-sveltekit/server'
-import type {Handle} from '@sveltejs/kit';
+import type { Handle } from '@sveltejs/kit';
 
+export const handle: Handle = (async ({ event, resolve }) => {
+	const defaultLang = event?.request?.headers?.get('accept-language')?.split(',')[0];
+  const language = defaultLang || 'en';
 
-export const handle: Handle = ({event, resolve}) => {
-  const defaultLang = event.request.headers.get('accept-language')?.split(',')[0];
-  const language = defaultLang ? defaultLang: 'en'
   const runtime = initSvelteKitServerRuntime({ language, referenceLanguage: 'en', languages })
 	addRuntimeToLocals(event.locals, runtime)
-  return resolve(event);
-};
+  
+	const response = await resolve(event);
+  return response;
+})
+
+
 
 
 export const handleError = async ({ error, event }) => {
