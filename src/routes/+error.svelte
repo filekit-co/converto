14f624ce4b78bdfc4fd8b95b5b6e72b17cb69b3c
minkj1992 <minkj1992@gmail.com>
2023-07-10 22:00:47 +0900

    style: update error page style

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -7,16 +7,16 @@
   <div
     class="h-screen w-full flex flex-col justify-center items-center bg-base-900"
   >
-    <h1 class="text-9xl font-extrabold text-neutral-700 tracking-widest">
+    <h1 class="text-9xl font-extrabol text-neutral-700 tracking-widest">
       {$page.status}
     </h1>
-    <div class="bg-accent px-2 text-sm rounded absolute rotate-12">
+    <div class="bg-accent px-2 text-sm rounded-full">
       {$page.error?.message || 'Filekit.co'}
     </div>
     <!-- button -->
-    <div class="join join-vertical md:join-horizontal">
-      <a href={`/${language}`} class="join-item btn btn-accent"> Go Home </a>
-      <a href={`/${language}/send-email`} class="join-item btn btn-neutral-500">
+    <div class="pt-5 join join-vertical md:join-horizontal">
+      <a href={`/${language}`} class="join-item btn btn-outline"> Go Home </a>
+      <a href={`/${language}/send-email`} class="join-item btn btn-outline">
         Contact us
       </a>
     </div>
