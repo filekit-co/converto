d0613e29cefe45ea2acfaea682cd2978747055df
minkj1992 <minkj1992@gmail.com>
2023-08-28 16:50:18 +0900

    delete: ads

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,53 +19,6 @@
   $: $errorMessage;
 </script>
 
-<svelte:head>
-  <!-- @ads: Banner 468x60 -->
-  <!-- @ads: Banner 468x60 -->
-  <script type="text/javascript">
-    atOptions = {
-      key: 'd8f239bc39bef71459fcda80043f8868',
-      format: 'iframe',
-      height: 60,
-      width: 468,
-      params: {}
-    };
-    document.write(
-      '<scr' +
-        'ipt type="text/javascript" src="http' +
-        (location.protocol === 'https:' ? 's' : '') +
-        '://www.profitablecreativeformat.com/d8f239bc39bef71459fcda80043f8868/invoke.js"></scr' +
-        'ipt>'
-    );
-  </script>
-  <!-- @ads: Banner 300x250 -->
-  <!-- @ads: Banner 300x250 -->
-  <script type="text/javascript">
-    atOptions = {
-      key: 'f753a0e63f622ab13acf363340b1493b',
-      format: 'iframe',
-      height: 250,
-      width: 300,
-      params: {}
-    };
-    document.write(
-      '<scr' +
-        'ipt type="text/javascript" src="http' +
-        (location.protocol === 'https:' ? 's' : '') +
-        '://www.profitablecreativeformat.com/f753a0e63f622ab13acf363340b1493b/invoke.js"></scr' +
-        'ipt>'
-    );
-  </script>
-  <!-- @ads: Native banner-->
-  <!-- @ads: Native banner-->
-  <script
-    async
-    data-cfasync="false"
-    src="//pl20385591.highcpmrevenuegate.com/4c6464626ad7dfabdf46e641ba81dca5/invoke.js"
-  ></script>
-  <div id="container-4c6464626ad7dfabdf46e641ba81dca5" />
-</svelte:head>
-
 <Header />
 
 {#if $page.error}
